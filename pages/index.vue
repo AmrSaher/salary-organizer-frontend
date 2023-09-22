1a0d7d19b2e1d8703c064f2417dd0490f5e47e42
Amr Saher <amrosaher1234@gmail.com>
2023-09-22 19:14:22 +0300

    create login page

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,21 @@
     <div
         class="
             Main w-full min-h-screen flex flex-col justify-between
-            items-center py-10
+            items-center py-10 px-5
         "
     >
         <span></span>
         <h1 class="text-[#5c46ea] text-4xl font-bold">Salary Organizer</h1>
-        <div class="flex flex-col w-full px-5 gap-2">
+        <div class="flex flex-col w-full gap-2">
             <NuxtLink
-                to="/home"
+                to="/login"
                 class="
                     w-full px-7 py-4 bg-[#5c46ea] text-white
                     text-center rounded-md font-semibold text-md
                 "
             >Login</NuxtLink>
             <NuxtLink
-                to="/"
+                to="/register"
                 class="
                     w-full px-7 py-4 bg-[#47445e] text-white
                     text-center rounded-md font-semibold text-md
---
+++ pages/login.vue
@@ -0,0 +1,74 @@
+<template>
+    <div
+        class="
+            Login w-full min-h-screen flex flex-col justify-between
+            py-10 px-5 text-white
+        "
+    >
+        <div class="flex flex-col">
+            <h2 class="text-lg font-semibold">Welcome back to salary organizer</h2>
+            <p class="text-sm text-gray-300 mb-3">Make you life better</p>
+            <form
+                @submit.prevent="handleLoginSubmit"
+                class="
+                    w-full flex flex-col gap-3
+                "
+            >
+                <input
+                    type="email"
+                    placeholder="Email Address"
+                    v-model="formData.email"
+                    class="
+                        w-full p-4 rounded-md outline-none text-md
+                        bg-[#363447] border-2 border-[#47445e]
+                    "
+                >
+                <input
+                    type="password"
+                    placeholder="Password"
+                    v-model="formData.password"
+                    class="
+                        w-full p-4 rounded-md outline-none text-md
+                        bg-[#363447] border-2 border-[#47445e]
+                    "
+                >
+            </form>
+        </div>
+        <div class="flex flex-col gap-2">
+            <button
+                @click="handleLoginSubmit"
+                class="
+                    w-full px-7 py-4 bg-[#5c46ea] text-white
+                    text-center rounded-md font-semibold text-md
+                "
+            >Login</button>
+            <div class="w-full flex justify-between items-center">
+                <span class="w-full h-[1px] bg-[#47445e]"></span>
+                <span class="text-[#47445e] mx-2">OR</span>
+                <span class="w-full h-[1px] bg-[#47445e]"></span>
+            </div>
+            <NuxtLink
+                to="/register"
+                class="
+                    w-full px-7 py-4 bg-[#47445e] text-white
+                    text-center rounded-md font-semibold text-md
+                "
+            >Register</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    layout: 'start',
+})
+
+const formData = ref({
+    email: '',
+    password: '',
+})
+
+const handleLoginSubmit = () => {
+    //
+}
+</script>
